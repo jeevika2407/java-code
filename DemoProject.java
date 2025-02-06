79efbf76f46a5d069cc2cb8da8d4f54918f2e224
jeevika2407 <144089941+jeevika2407@users.noreply.github.com>
2025-02-06 22:12:51 +0530

    DemoProject.java

--- DemoProject.java
+++ DemoProject.java
@@ -1,5 +1,13 @@
+import java.util.*;
 public class DemoProject{
-	public static void main(String args[]) {
-		System.out.println("Hello World");
-	}
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Usage: java DemoProject <name> <age>");
+            return;
+        }
+        String name = args[0];
+        int age = Integer.parseInt(args[1]);
+
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
 }
