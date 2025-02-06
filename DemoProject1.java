8ebbc300e7c6b245cbe8472fd005cea1d14db004
jeevika2407 <144089941+jeevika2407@users.noreply.github.com>
2025-02-06 22:03:34 +0530

    Update DemoProject1.java

--- DemoProject1.java
+++ DemoProject1.java
@@ -1,9 +1,9 @@
 import java.util.*;
 
-public class ass1 {
+public class DemoProject1{
     public static void main(String[] args) {
         if (args.length < 2) {
-            System.out.println("Usage: java ass1 <name> <age>");
+            System.out.println("Usage: java DemoProject1 <name> <age>");
             return;
         }
         String name = args[0];
